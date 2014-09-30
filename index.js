09269419194bde6e10437bcd371eca9345eec45e
Steve Lacy <me@slacy.me>
2014-09-30 10:55:13 -0700

    update to through2, clean code. 0.3.0

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 var open = require('open');
-var map = require('map-stream');
+var through = require('through2');
 var gutil = require('gulp-util');
 
 module.exports = function(src, opt) {
 
-  return map(function (file, cb){
+  return through.obj(function(file, enc, cb){
     if (file.isNull()) return cb(null, file); // pass along
     if (file.isStream()) src = file.path;
     if (!opt) opt = {};
     var cmd = gutil.template(src, {file:file});
-    
+
     if (!opt.app && !opt.url) {
       open(file.path);
-      return true;
+      return cb(null, file);
     }
     if(opt.url){
       open(opt.url, opt.app);
-      return true;
+      return cb(null, file);
     }
     // Run normally
     open(cmd, opt.app);
-    cb(null, true);
+    cb(null, file);
   });
 };
