9a69d051d33be7c91a2e9564552d9ce289989b1f
Steve Lacy <me@slacy.me>
2015-07-14 00:36:19 -0700

    Clean index.js, remove unneeded functions.
    `open` handles all filetypes, no need to check types.
    clean error formatting, no if statements after checking.

--- index.js
+++ index.js
@@ -4,76 +4,35 @@ var open = require('open');
 var through = require('through2');
 var gutil = require('gulp-util');
 
-var log = gutil.log;
 var colors = gutil.colors;
 
 var PLUGIN_NAME = 'gulp-open';
 
-// The acceptable uri formats for opening
-var ACCEPTABLE_URI_FORMATS = ['/', 'file://', 'http://', 'https://'];
+module.exports = function(opts) {
 
-module.exports = function(src, opt) {
-
-  opt = opt || {};
-  var initError = null;
-  if (!src && !opt.uri) {
-    initError = 'URI is missing or incorrect! Use the src or the options to indicate a uri' +
-      ACCEPTABLE_URI_FORMATS;
-  }
+  opts = opts || {};
 
   return through.obj(function(file, enc, cb) {
 
-    if (initError) {
-      return cb(new gutil.PluginError(PLUGIN_NAME, initError));
-    }
-    if (file.isNull()) {
-      return cb(null, file); // pass along
-    }
-    if (file.path)  {
-      opt.uri = file.path;
+    if (file.isNull() && !opts.uri) {
+      return cb(new gutil.PluginError(PLUGIN_NAME, 'URI is missing or incorrect'));
     }
 
-    var selectedType = 'none';
-    // If the uri was not passed either by the stream nor by the options
-    var indexOfAnyFormat = _getIndexesOfAcceptableFormats(opt.uri);
-    if (indexOfAnyFormat === -1) {
-      return cb(new gutil.PluginError(PLUGIN_NAME, 'URI is missing or incorrect! Please ' +
-        'use one of ' + ACCEPTABLE_URI_FORMATS));
-
-    } else {
-      // If the type is the index value in the range of files
-      selectedType = indexOfAnyFormat <= 1 ? 'file' : 'url';
+    if (file.path && !opts.uri)  {
+      opts.uri = file.path;
     }
 
-    if (opt.app) {
-      log(colors.blue('Opening the', selectedType, colors.green(opt.uri), 'using the app',
-        colors.green(opt.app)));
+    if (opts.app) {
+      gutil.log(colors.blue('Opening', colors.green(opts.uri), 'using the app',
+        colors.green(opts.app)));
       // Open with the given app
-      open(opt.uri, opt.app);
- 
-    } else {
-     log(colors.blue('Opening the', selectedType, colors.green(opt.uri), 'using the',
-        colors.green('default OS app')));
-     // Open with the default app defined by the os
-     open(opt.uri);
+      return open(opts.uri, opts.app);
+
     }
+    gutil.log(colors.blue('Opening', colors.green(opts.uri), 'using the',
+      colors.green('default OS app')));
+    // Open with the default app defined by the os
+    open(opts.uri);
+
   });
 };
-
-/**
- * In addition to the regular path
- * > _followsFormat('github.com')   > _followsFormat('http://github.com')
- * false                            true
- * > _followsFormat('file://dsdsd') > _followsFormat('/usr/bin') 
- * true                             true
- *
- * @return The index of which type of the acceptable types the uri is from.
- */
-function _getIndexesOfAcceptableFormats(uri) {
-  return ACCEPTABLE_URI_FORMATS.map(function(formatPrefix) {
-    return uri.indexOf(formatPrefix) === 0;
-
-  }).reduce(function(acc, value, index) {
-    return value ? index : acc;
-  }, false);
-}
