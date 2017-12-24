582f888222e9a227632229547b0f1868b428fe5c
Steve Lacy <me@slacy.me>
2017-12-23 23:21:14 -0700

    Allow for file streams with `read:false` - closes #31

--- index.js
+++ index.js
@@ -2,11 +2,9 @@
 
 var open = require('open');
 var through = require('through2');
-var gutil = require('gulp-util');
+var log = require('plugin-log');
 
-var colors = gutil.colors;
-
-var PLUGIN_NAME = 'gulp-open';
+var colors = log.colors;
 
 module.exports = function(opts) {
   opts = opts || {};
@@ -14,24 +12,19 @@ module.exports = function(opts) {
   return through.obj(function(file, enc, cb) {
     var uri = opts.uri;
 
-    if (file.isNull() && !uri) {
-      return cb(new gutil.PluginError(PLUGIN_NAME,
-        'URI is missing or incorrect'));
-    }
-
     if (file.path && !uri)  {
       uri = file.path;
     }
 
     if (opts.app) {
-      gutil.log(colors.blue('Opening', colors.green(uri), 'using the app',
+      log(colors.blue('Opening', colors.green(uri), 'using the app',
         colors.green(opts.app)));
       // Open with the given app
       open(uri, opts.app);
       return cb(null, file);
 
     }
-    gutil.log(colors.blue('Opening', colors.green(uri), 'using the',
+    log(colors.blue('Opening', colors.green(uri), 'using the',
       colors.green('default OS app')));
     // Open with the default app defined by the os
     open(uri);
