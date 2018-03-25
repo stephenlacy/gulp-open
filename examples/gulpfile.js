4ca16b000002d352dd230fd5685c1a84a6845b61
TheDancingCode <thomasvantuycom@protonmail.com>
2018-03-25 21:28:01 +0000

    Fix `app` option after switch to `opn` (#34)
    * Fix `app` option
    
    * Correct browser example

--- examples/gulpfile.js
+++ examples/gulpfile.js
@@ -18,9 +18,9 @@ var browser = os.platform() === 'linux' ? 'google-chrome' : (
   os.platform() === 'darwin' ? 'google chrome' : (
   os.platform() === 'win32' ? 'chrome' : 'firefox'));
 
-gulp.task('browser', function(cb){
+gulp.task('browser', function(){
   gulp.src('./*.html')
-  .pipe(open({app: browser}, cb));
+  .pipe(open({app: browser}));
 });
 
 // Simple usage, no options.
